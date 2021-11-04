c52b1932ea22278d6ab9e2ce0f566a465b23a9af
lilinghai <jidushanlixi@163.com>
2021-11-04 12:30:54 +0800

    refine values

--- resource/resource/default.zz.lua
+++ resource/resource/default.zz.lua
@@ -1,5 +1,5 @@
 tables = {
-    rows = { 1, 10, 20, 25, 50, 75, 100 },
+    rows = { 1, 10, 20, 25, 50, 75, 100 ,1000},
     charsets = { 'utf8mb4 collate=utf8mb4_general_ci', 'utf8mb4 collate = utf8mb4_bin', 'latin1', 'binary' },
     partitions = { 'undef' },
 };
@@ -7,8 +7,8 @@ tables = {
 fields = {
     -- The four BLOB types are TINYBLOB, BLOB, MEDIUMBLOB, and LONGBLOB.
     -- The four TEXT types are TINYTEXT, TEXT, MEDIUMTEXT, and LONGTEXT.
-    types = { 'int',
-              'char(20)', 'varchar(20)', 'binary(20)', 'varbinary(20)',
+    types = { 'int', 'datetime', 'date', 'timestamp', 'float', 'double',
+              'char(20)', 'varchar(20)',
         --              'enum', 'set',
         -- todo key blob and text prefix
         --      'tinyblob','blob','mediumblob','longblob',
@@ -19,7 +19,10 @@ fields = {
 }
 
 data = {
-    --   enum = { '"y"', '"b"', '"Abc"', '"null"' },
-    numbers = { 'null', 'tinyint', '0', '-1', '1', '10', '20' },
+       enum = { '"y"', '"b"', '"Abc"', '"null"' ,'"1"','"2"','"0"'},
+    numbers = { 'null','12', '0', '-1', '1', '10', '4', '12.991', '1.009', '-9.183' },
     strings = { 'null', 'english', 'english', 'english', 'english', 'english', 'english', 'english' },
+    datetime = { 'null', 'datetime' },
+    timestamp = { 'null', 'datetime' },
+    date = { 'null', 'date' },
 }
